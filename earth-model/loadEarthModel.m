f61131851985a2f5440d65eee352ad96e5879cd0
Matt Werner <m.werner@vt.edu>
2020-12-29 01:20:28 -0500

    Add cache system for loading Earth model
    Cache system saves/reads a small .mat file containing parameters
    defining the Earth model that is compared against current defining
    parameters. Those parameters that are the same simply reuse the model
    defined from the previous run of rtraj.m.
    Note: The working directory currently must be that of rtraj.m for the
    cache to work properly.

---
+++ earth-model/loadEarthModel.m
@@ -0,0 +1,40 @@
+% 
+% Matt Werner (m.werner@vt.edu) - Dec 28, 2020
+% 
+% Load the earth model while attempting to use the cache if possible.
+
+% SCRIPT - Creates flags to determine which models may be used again from
+% the rtraj.m workspace (if already populated by a previous simulation) and
+% which require reloading
+cache
+
+% Only load the models and coefficients usd to define the earth that are
+% either not currently stored in the rtraj.m workspace or are to be updated
+% from the previous simulation
+% 
+% Load or cache ellipsoid parameters
+if (flag_loadEllipsoid)
+    [GM, Req, Rpo, f, e, w] = defineEllipsoidParameters(earthModel);
+end
+% Load or cache the geopotential harmonic coefficients
+if (flag_loadGravityField)
+    [nG, mG, ~, Cnm, Snm, ~, ~] = loadGravitationalCoefficients(gravityDegree, gravityOrder, gravityModel);
+    [Cnm, Snm] = updateGravitationalCoefficients(JDLaunch, nG, mG, Cnm, Snm);
+else
+    disp("Caching gravity model...")
+    fprintf("Loaded gravity model\n\n")
+end
+% Load or cache the magnetic field potential harmonic coefficients
+if (flag_loadMagneticField)
+    [nM, mM, gnm, hnm, dgnmdt, dhnmdt] = loadMagneticCoefficients(magneticDegree, magneticOrder);
+    [gnm, hnm] = updateMagneticCoefficients(JDLaunch, gnm, hnm, dgnmdt, dhnmdt);
+else
+    disp("Caching magnetic model...")
+    fprintf("Loaded magnetic model\n\n")
+end
+% Load or cache the terrain
+if (flag_loadTerrain)
+    [longitudes, geodeticLatitudes, WGS84ToGeoid, GeoidToTerrain, WGS84ToTerrain] = loadTerrain(terrainAngleUnits);
+else
+    fprintf("Terrain cached from previous simulation.\n\n")
+end
--- scripts/cache/cache.m
+++ scripts/cache/cache.m
@@ -5,10 +5,92 @@
 % rtraj. The most significant portion of time, by far, is spent in loading
 % the terrain.
 
-% Create a flag to indicate that the loading process should load the
-% terrain matrices, incurring lots of run time.
-flag_loadTerrainMats = true;
-if (exist('GeoidToTerrain', 'var'))
-    % Switch the flag to be false
-    flag_loadTerrainMats = false;
+% Set default flags indicating that no cache currently exists and
+% all components of the model determining the ellipsoid parameters,
+% gravitational and magnetic fields, and terriain shall be loaded as if for
+% the first time
+flag_previous_earth_model_cache_exists = false;
+flag_rtraj_workspace_populated = true;
+flag_loadEllipsoid = true;
+flag_loadGravityField = true;
+flag_loadMagneticField = true;
+flag_loadTerrain = true;
+
+% Attempt to load previous inputs for the earth model into the workspace.
+% This process is justified because the load only occurs once per run of
+% rtraj.m and the resulting table contains few entries, so the load is fast
+try
+    % Obtain the path that points to where the cache is
+    pathToMain = pwd;
+    if (ispc)
+        relativePathToCache = '\cache\previous_earth_model.mat';
+    elseif (ismac || isunix)
+        relativePathToCache = '/cache/previous_earth_model.mat';
+    else
+        error("Platform not supported.")
+    end
+    pathToCache = strcat(pathToMain, relativePathToCache);
+    % Attempt to load the cache (may not exist)
+    previous_earth_model = load(pathToCache);
+    flag_previous_earth_model_cache_exists = true;
+catch error_in_cache
+    % Check possible causes for error
+    switch error_in_cache.identifier
+        case 'MATLAB:load:couldNotReadFile'
+            % Assume the workspace is empty since there is no cache
+            flag_rtraj_workspace_populated = false;
+            % Create a new cache file since one currently doesn't exist and
+            % carry on to load the terrain
+            save(pathToCache, 'earthModel', 'gravityModel', 'gravityDegree', ...
+                'gravityOrder', 'magneticDegree', 'magneticOrder', ...
+                'terrainAngleUnits')
+        otherwise
+            rethrow(error_in_cache)
+    end
+    return
 end
+
+% Check if the models exist in the rtraj.m workspace. To avoid issues with
+% complexity and naming schemes, only check if the variable 'Req' exists
+% and assume the others haven't been deleted from the previous run (if
+% there was one). Therefore, assume that the other variables are there as
+% well if just one of them is there since they all load together
+if (evalin('base', '~exist("Req", "var")'))
+    flag_rtraj_workspace_populated = false;
+end
+
+
+
+if (flag_rtraj_workspace_populated)
+    % Determine which models require loading/processing by comparing currently
+    % requested model parameters to previous model parameters
+    % 
+    % Check if the requested ellipsoid is different from the one used in the
+    % previous run
+    if (strcmp(earthModel, previous_earth_model.earthModel))
+        flag_loadEllipsoid = false;
+    end
+    % Check if the requested gravitational field is different from the one used
+    % in the previous run
+    if (gravityDegree == previous_earth_model.gravityDegree && ...
+            gravityOrder == previous_earth_model.gravityOrder && ...
+            gravityModel == previous_earth_model.gravityModel)
+        flag_loadGravityField = false;
+    end
+    % Check if the requested magnetic field is different from the one used in
+    % the previous run
+    if (magneticDegree == previous_earth_model.magneticDegree && ...
+            magneticOrder == previous_earth_model.magneticOrder)
+        flag_loadMagneticField = false;
+    end
+    % Check if the requested angular units for the terrain are different from
+    % the ones used in the previous run
+    if (strcmp(terrainAngleUnits, previous_earth_model.terrainAngleUnits))
+        flag_loadTerrain = false;
+    end
+end
+
+% Update the cache
+save(pathToCache, 'earthModel', 'gravityModel', 'gravityDegree', ...
+    'gravityOrder', 'magneticDegree', 'magneticOrder', ...
+    'terrainAngleUnits')
