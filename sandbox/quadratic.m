8eb9a0eee69e530ee3c3e510c3a34110952361f8
Ryan Shaffer <rwshaffer@vt.edu>
2021-02-28 13:28:33 -0500

    fix quadratic formula

--- sandbox/quadratic.m
+++ sandbox/quadratic.m
@@ -1,7 +1,7 @@
 function [x1,x2] = quadratic(a,b,c)
 %quadratic formula. Test by Ryan Shaffer
-x1 = -b + sqrt(b^2 - 4*a*c);
-x2 = -b - sqrt(b^2 - 4*a*c);
+x1 = (-b + sqrt(b^2 - 4*a*c))/(2*a);
+x2 = (-b - sqrt(b^2 - 4*a*c))/(2*a);
 
 end
 
